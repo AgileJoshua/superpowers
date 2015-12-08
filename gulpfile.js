afd4d82974332185e677a361260410ba34314e3f
Joshua Anthony <joshua.anthony@habanero.se>
2015-12-08 22:01:29 +0000

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var zip = require("gulp-zip");
 
-gulp.task("deploy", ["build"], function() {
+gulp.task("default", ["build"], function() {
     return gulp.src(["**"])
         .pipe(zip("deploy.zip"))
         .pipe(gulp.dest("../"));
